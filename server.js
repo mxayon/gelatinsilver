324ebc943564f97fcf615430e79d5ec45e4cdbc9
mxayon <mxnkpl@gmail.com>
2019-06-20 16:14:26 -0700

    adds static file hosting endpoint

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 var express = require('express');
 var app = express();
 
+// parse incoming URL encoded from data
+// populate req.body obj
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
+// allow cross origin requests
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+// ************ DATABASE **********/
+var db = require('./models');
+
+
 // temp data
 var cat = [
   'Pumpkin',
@@ -15,7 +32,7 @@ var tacotruck = [
   'Utensils'
 ];
 
-// ROUTES
+// ***************  ROUTES ********* /
 
 // Serve Static Files
 // '/images', '/scripts', '/styles'
@@ -23,12 +40,12 @@ app.use(express.static('public'));
 
 
 // HTML endpoints
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/', function homepage(req, res) {
+  res.sendFile(__dirname + '/views/index.html');
 });
 
 
 // SERVER START
-app.listen(process.env.PORT || 3000, function () {
+app.listen(3000, function () {
   console.log('Info beaming @ http://localhost:3000/');
 });
