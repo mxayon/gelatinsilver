718893667276d8989c92e2cb876a385c26266387
mxayon <mxnkpl@gmail.com>
2019-06-21 10:30:47 -0700

    adds mongodb local host to indexJS

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
 var mongoose = require('mongoose');
-mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/personal-api");
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/gelatin");
 
 // module.exports.Photo = require("./photo.js");
