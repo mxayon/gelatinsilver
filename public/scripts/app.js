2b8fea210e466c0aad7d1a61a6d191f2b31d8610
mxayon <mxnkpl@gmail.com>
2019-06-21 10:31:40 -0700

    connects appJS with doc ready and cat taco api

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,21 @@
 console.log(" Sanity check - Multiphoton Ionization from the inner orbitals -> for ro-vibrational levels of an excited electronic state")
+
+$(document).ready(function(){
+
+  //api connect for temp data on server
+  $.ajax({
+  method: 'GET',
+  url: '/api/cat',
+  success: handleSuccess,
+  error: handleError
+  });
+
+  $.ajax({
+  method: 'GET',
+  url: '/api/tacotruck',
+  success: handleSuccess,
+  error: handleError
+  });
+
+
+});
