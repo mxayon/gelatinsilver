ed1dc9e30584722ec475500a5a863e68b1560a4f
mxayon <mxnkpl@gmail.com>
2019-06-21 17:03:44 -0700

    adds seed data to test

---
+++ seed.js
@@ -0,0 +1,74 @@
+var db = require('./models');
+
+
+
+var photos_list = [
+  {
+  image: "images/goldengatebridgenitelight.jpg",
+  type: "Film",
+  location: "Golden Gate Bridge, SF",
+  film_type: "ISO 400",
+  settings: " ",
+  camera: "Cannon AE-1 Program / 50mm",
+  description: "Late night lights"
+  },
+  {
+  image: "images/prstepsbeachwaves.jpg",
+  type: "Film",
+  location: "Rincon, Puerto Rico",
+  film_type: "ISO 400",
+  settings: " ",
+  camera: "Cannon AE-1 Program / 50mm",
+  description: "Waves on steps beach in Rincon"
+  },
+  {
+  image: "images/goldengatebridge.jpg",
+  type: "Film",
+  location: "San Francisco",
+  film_type: "ISO 200",
+  settings: " ",
+  camera: "Cannon AE-1 Program / 50mm",
+  description: "Afternoon time crossing the bridge"
+  },
+  {
+  image: "images/groundsrcc.jpg",
+  type: "Film",
+  location: "San Rafael, CA",
+  film_type: "ISO 200",
+  settings: " ",
+  camera: "Cannon AE-1 Program / 50mm",
+  description: " "
+  },
+  {
+  image: "images/srccfaucetwaterdrip.jpg",
+  type: "Film",
+  location: "San Rafael, CA",
+  film_type: "ISO 200",
+  settings: " ",
+  camera: "Cannon AE-1 Program / 50mm",
+  description: "waves on steps beach in Rincon"
+  },
+  {
+  image: "images/srcclongpostISO200.jpg",
+  type: "Film",
+  location: "San Rafael, CA",
+  film_type: "ISO 100",
+  settings: " ",
+  camera: "Cannon Program AE-1, 50mm",
+  description: "Posts"
+  }
+  ];
+//////remove messy seeds
+db.Photo.remove({}, function(err, photos){
+  if (err) {
+    console.log("error occured in remove", err);
+  } else {
+    console.log('removed all photos');
+    /////////create new records for photos
+    db.Photo.create(photos_list, function(err, photos){
+      if (err) {return console.log('err', err); }
+      console.log("created", photos.length, "photos");
+      process.exit();
+    });
+  }
+});
