755a0707b50d01c5b3c7f0eaea67dd966ea2d84b
mxayon <mxnkpl@gmail.com>
2019-06-22 02:56:13 -0700

    updates comment for photos/all

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -18,12 +18,13 @@ $(document).ready(function(){
   // new photo form
   // delete photo
   // update photo with comment
+
   // helper function to render all posts to view
   // note: we empty and re-render the collection each time our post data changes
   function render () {
     // empty existing posts from view
     $photosList.empty();
-    // pass `allBooks` into the template function
+    // pass photos into the template function
     var photosHtml = template({ photos: allPhotos });
     // append html to the view
     $photosList.append(photosHtml);
