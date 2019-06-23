70b591a0c27b2a88394b20b8f14680720d44bbea
mxayon <mxnkpl@gmail.com>
2019-06-23 11:51:40 -0700

    reverts mongo lab connect to local host

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
 var mongoose = require('mongoose');
-mongoose.connect( process.env.MONGODB_URI || "mongodb://<dbuser>:<dbpassword>@ds341837.mlab.com:41837/heroku_6qm6hd64", { useNewUrlParser: true });
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/gelatinsilver", { useNewUrlParser: true });
 
 module.exports.Photo = require("./photo.js");
