35b372b517ab18971ca89b4464acfa731930723f
mxayon <mxnkpl@gmail.com>
2019-07-01 21:36:36 -0700

    fixes prod to uri

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
-mongoose.connect( process.env.PROD_MONGODB || "mongodb://localhost/gelatinsilver", { useNewUrlParser: true });
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/gelatinsilver", { useNewUrlParser: true });
 
 module.exports.Photo = require("./photo.js");
